5506bdedaa191c68ee1735552410acc258e63ddc
Geo <gaa39@mail.aub.edu>
2017-06-09 12:48:17 +0300

    Added Registration Controller

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -1,51 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: saugo
- * Date: 9/2/16
- * Time: 1:04 PM
- */
 
 namespace AppBundle\Controller;
-
-
-use AppBundle\Entity\Users;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use AppBundle\Form\addUsers;
-use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\UserType;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
-class addUser extends Controller
+class RegistrationController extends Controller
 {
     /**
-     * @Route("/addUser", name="addUser")
+     * @Route("/register", name="user_registration")
      */
-    public function  addUser(Request $request)
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $em)
     {
         // 1) build the form
-        $user = new Users();
-        $form = $this->createForm(addUsers::class, $user);
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
 
         // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
             // 3) Encode the password (you could also do this via Doctrine listener)
-            $password = $this->get('security.password_encoder')
-                ->encodePassword($user, $user->getPassword());
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
 
             // 4) save the User!
-            $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
+
             // ... do any other work - like sending them an email, etc
             // maybe set a "flash" success message for the user
 
             return $this->redirectToRoute('login');
         }
+
         return $this->render(
-            'agriApp/addUser.html.twig',
+            ':agriApp:addUser.html.twig',
             array('form' => $form->createView())
         );
     }
