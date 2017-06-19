784444215e40711a4372604826c0ac9072474b15
Nappa <ali.nabaa@saugo360.com>
2017-06-19 12:45:58 +0300

    CRUD

--- src/AppBundle/Controller/CompanyTypeAttributeController.php
+++ src/AppBundle/Controller/CompanyTypeAttributeController.php
@@ -2,10 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\CompanyTypeAttribute;
 use Doctrine\DBAL\DBALException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CompanyTypeAttributeController extends Controller
 {
@@ -19,7 +22,9 @@ class CompanyTypeAttributeController extends Controller
      */
     public function CompanyTypeAttributes()
     {
-        return $this->render('agriApp/CompanyTypeAttribute/companyTypeAttributesRecords.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $companyTypes = $em->getRepository('AppBundle:CompanyType')->findAll();
+        return $this->render('agriApp/CompanyTypeAttribute/companyTypeAttributesRecords.html.twig',['companyTypes'=>$companyTypes]);
     }
 
     /**
@@ -46,6 +51,28 @@ class CompanyTypeAttributeController extends Controller
         } catch (DBALException $e) {
             return new JsonResponse(array('status' => 'error', 'message' => 'Can\'t delete record'));
         }
+    }
+
+    /**
+     * @Route("/addCompanyTypeAttribute",name="addCompanyTypeAttribute")
+     */
+    public function addCompanyTypeAttribute(Request $request)
+    {
+        if ($request->getMethod() === 'POST')
+        {
+            if ($request->request)
+            {
+                $em = $this->getDoctrine()->getManager();
+                $companyType = $em->getRepository('AppBundle:CompanyType')->find($request->request->get('companyType'));
+                $companyTypeAttribute = new CompanyTypeAttribute();
+                $companyTypeAttribute->setCompanyType($companyType);
+                $companyTypeAttribute->setName($request->request->get('attribute'));
+                $em->persist($companyTypeAttribute);
+                $em->flush();
+                return new Response('Added Attribute Successfully');
+            }
+        }
+        return new Response('Add Attribute Fail');
     }
 
 }
